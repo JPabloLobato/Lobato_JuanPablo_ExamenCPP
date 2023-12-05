6378e88513c1e538f4c0480486262a45ac1d5970
JPabloLobato <jlobagon@myuax.com>
2023-12-05 11:58:50 +0100

    Parte 2.5

--- Parte2.cpp
+++ Parte2.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <variant>
+
+using Symbol = std::variant<int, float, std::string>;
 
 class Enviroment {
 private:
-    std::map<std::string, int> symbolTable;
+    std::map<std::string, Symbol> symbolTable;
 public:
-    void insert(const std::string& symbol, int value) {
+    void insert(const std::string& symbol, Symbol value) {
         auto it = symbolTable.find(symbol);
         if (it != symbolTable.end() && it->second != value) {
-            throw std::runtime_error("Symbol already exists");
-    }
+            throw std::runtime_error("Symbol ya existe");
+        }
         symbolTable[symbol] = value;
     }
-    int lookup(const std::string& symbol) const {
+    Symbol lookup(const std::string& symbol) const {
         auto it = symbolTable.find(symbol);
-        if (it != symbolTable.end()) {
+        if (it == symbolTable.end()) {
             return it->second;
         } else {
-            throw std::runtime_error("Symbol not found");
+            throw std::runtime_error("Symbol no existe");
         }
     }
-    std::map<std::string, int> getSymbolTable() const {
+    std::map<std::string, Symbol> getSymbolTable() const {
         return symbolTable;
     }
 };
@@ -29,22 +32,35 @@ int main () {
     Enviroment env;
     try {
         env.insert("symbol1", 1);
-        env.insert("symbol2", 2);
-        env.insert("symbol1", 3);
+        env.insert("symbol2", 2.0f);
+        env.insert("symbol3", "hola");
     } catch (const std::runtime_error& e) {
         std::cout << "Error: " << e.what() << std::endl;
     }
     try {
-        int value = env.lookup("symbol1");
-        std::cout << "Valor de symbol1: " << value << std::endl;
+        Symbol value = env.lookup("symbol1");
+        std::visit([](auto&& arg) {
+            std::cout << "Valor de symbol1: " << arg << std::endl;
+        }, value);
     } catch (const std::runtime_error &e ) {
         std::cout << "Error: " << e.what() << std::endl;
     }
     try {
-        int value = env.lookup("symbol3");
-        std::cout << "Valor de symbol3: " << value << std::endl;
+        Symbol value = env.lookup("symbol2");
+        std::visit([](auto&& arg) {
+            std::cout << "Valor de symbol2: " << arg << std::endl;
+        }, value);
     } catch (const std::runtime_error &e ) {
         std::cout << "Error: " << e.what() << std::endl;
     }
+    try {
+        Symbol value = env.lookup("symbol3");
+        std::visit([](auto&& arg) {
+            std::cout << "Valor de symbol3: " << arg << std::endl;
+        }, value);
+    } catch (const std::runtime_error &e ) {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
+
     return 0;
 }
