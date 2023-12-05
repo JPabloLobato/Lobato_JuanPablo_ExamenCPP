513feaa31d1ba6f08d80dcbff209d6efaefdc751
JPabloLobato <jlobagon@myuax.com>
2023-12-05 10:59:08 +0100

    Parte 2.2 terminado

--- Parte2.cpp
+++ Parte2.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <map>
 #include <string>
 
@@ -5,15 +6,20 @@ class Enviroment {
 private:
     std::map<std::string, int> symbolTable;
 public:
-    void insert (const std::string &symbol, int value) {
+    void insert(const std::string& symbol, int value) {
         symbolTable[symbol] = value;
+    }
+
+    std::map<std::string, int> getSymbolTable() const {
+        return symbolTable;
     }
 };
 int main () {
     Enviroment env;
     env.insert("symbol1", 1);
     env.insert("symbol2", 2);
-    for (const auto& pair : env.symbolTable) {
+    std::map<std::string, int> symbolTable = env.getSymbolTable();
+    for (const auto& pair : symbolTable) {
         std::cout << pair.first << ": " << pair.second << std::endl;
     }
     return 0;
