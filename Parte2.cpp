b5c4d1d3ab4f23b2b5f8ec9afa7dff977265a59d
JPabloLobato <jlobagon@myuax.com>
2023-12-05 10:53:06 +0100

    Parte 2.2

--- Parte2.cpp
+++ Parte2.cpp
@@ -5,5 +5,16 @@ class Enviroment {
 private:
     std::map<std::string, int> symbolTable;
 public:
-
+    void insert (const std::string &symbol, int value) {
+        symbolTable[symbol] = value;
+    }
 };
+int main () {
+    Enviroment env;
+    env.insert("symbol1", 1);
+    env.insert("symbol2", 2);
+    for (const auto& pair : env.symbolTable) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+    return 0;
+}
